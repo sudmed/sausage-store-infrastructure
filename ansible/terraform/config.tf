90115de24f96a7301ac65dea7bba68467085f0be
Дмитрий Пашков <sudmed-ru@yandex.ru>
2022-07-26 22:40:50 +0000

    Add ansible & terraform files for deploy on 2 VMs

---
+++ ansible/terraform/config.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+      version = "0.61.0"
+    }
+  }
+}
+
+provider "yandex" {
+  token     = var.IAM_token
+  cloud_id  = var.cloud_id
+  folder_id = var.folder_id
+  zone      = var.zone
+}
